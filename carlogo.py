86c96e24b967428afdf0d05f5d2f6f2876ec8dd2
kouchakun <seta.adachi@gmail.com>
2023-08-27 20:57:06 +0900

    size-change_carlogo.py

--- carlogo.py
+++ carlogo.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 classes = ["テスラ","メルセデスベンツ","ホンダ","トヨタ","レクサス","ポルシェ","日産","フォルクスワーゲン"]
-image_size = 28
+image_size = 150
 
 UPLOAD_FOLDER = "uploads"
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -37,7 +37,7 @@ def upload_file():
             filepath = os.path.join(UPLOAD_FOLDER, filename)
 
             #受け取った画像を読み込み、np形式に変換
-            img = image.load_img(filepath, grayscale=True, target_size=(image_size,image_size))
+            img = image.load_img(filepath, grayscale=False, target_size=(image_size,image_size))
             img = image.img_to_array(img)
             data = np.array([img])
             #変換したデータをモデルに渡して予測する
